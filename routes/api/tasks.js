045d373f40c0c20ef48050c22b7e3f18c959bce1
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-06-07 12:26:24 +0100

    Update API route completed

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -2,8 +2,9 @@ const express = require(`express`);
 const router = express.Router();
 const Task = require("../../models/Task");
 const { check, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
-// @route    POST api/tasksr
+// @route    POST api/tasks
 // @desc     Create a task
 // @access   Public
 router.post(`/`,
@@ -15,26 +16,76 @@ async(req,res) => {
 
 // If errors are found return error status 400 with an array of the errors
     if(!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()})
+        return res.status(400).json({errors: errors.array()});
     }
 
     try{
 // Create new Task
         const newTask = new Task({
             title: req.body.title
-        })
+        });
 // Save new task
         const task = await newTask.save();
 
 // Return new task in JSON
-        res.json(task)
+        res.json(task);
 
     }catch(err){
 // Return error message and status code 500 (Unexpected condition)
         console.error(err.message);
         res.status(500).send(`Server error`);
     }
-})
+});
 
+// @route    POST api/tasks/:taskId
+// @desc     Create a task
+// @access   Public
+router.put(`/:taskId`,
+check("title", "Please enter a title").notEmpty(),
+async(req,res) => {
+
+// Get errors from express-validator
+    const errors = validationResult(req);
+
+// If errors are found return error status 400 with an array of the errors
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+// Get data from the post body 
+    const {title, created_at} = req.body;
+
+// Turn the ID into ObjectID data type
+    const objectId = mongoose.Types.ObjectId(req.params.taskId);
+
+    try{
+
+// Use the findOneAndUpdate MongoDB method
+// It takes three arguments
+// 1 - Filter criteria
+// 2 - The updated document
+// 3 - A set of optional fields.
+// "new: true" returns the updated document after execution 
+        const task = await Task.findOneAndUpdate(
+            {
+                objectId
+            },
+            {
+                $set: {"title": title, "created_at": created_at}
+            },
+            {
+                new: true
+            }
+        )
+
+        res.json(task);
+
+    }catch(err){
+
+        // Return error message and status code 500 (Unexpected condition)
+        console.error(err.message);
+        res.status(500).send(`Server error`);
+    }
+})
 
 module.exports = router;
